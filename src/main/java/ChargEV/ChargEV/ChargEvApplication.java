a0b36df68f6c9e22c2fd9a04c72d2e9a146de74c
minjunss <jomj2214@gmail.com>
2025-09-10 06:32:02 +0900

    first commit

---
+++ src/main/java/ChargEV/ChargEV/ChargEvApplication.java
@@ -0,0 +1,13 @@
+package ChargEV.ChargEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargEvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ChargEV/ChargEV/ChargEvApplicationTests.java
@@ -0,0 +1,13 @@
+package ChargEV.ChargEV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
